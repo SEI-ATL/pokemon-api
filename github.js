ec6edf95bda6b8ffd3e774f4a5d4f6f40e379700
Amari James SEI <73178321+marjames98@users.noreply.github.com>
2020-11-10 02:00:08 -0500

    Update github.js

--- github.js
+++ github.js
@@ -1,7 +1,20 @@
+const container = document.querySelector('.container');
+
+
 fetch('https:api.github.com/users/marjames98')
 .then(response => {
     return response.json();
 })
 .then(githubData => {
-    console.log(githubData);
+    const marObj = {
+        bio:githubData.bio
+        username: githubData.login
+        name: githubData.name
+    }
+    const newElement = document.createElement('p');
+    newElement.textContent = marObj.name;
+    container.appendChild(newElement);
+});
+.catch(error => {
+    console.log(error);
 });
